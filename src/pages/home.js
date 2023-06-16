975d25df807bad30f73e49e625203d72c5ebb727
Nathan Patton <nathan@atlantamediaservices.com>
2023-06-16 14:24:43 -0400

    Maggies changes

--- src/pages/home.js
+++ src/pages/home.js
@@ -16,18 +16,30 @@ const Home = () => {
         {/* <span className="spanImage">Experience Authentic Japanese And Asian Fusion Cuisine</span> */}
       </div>
             <p className='closedForReno'>
+              <p>
               Mikado will be temporarily closing its doors for remodeling, starting from June 19th. This renovation is an exciting step forward in our journey towards 
               enhancing your dining experience.<br /><br />
+              </p>
+              <p>
               During this period, our team will be working tirelessly to transform our space into a vibrant and inviting Dim Sum and Chinese restaurant. We are thrilled 
               to announce that upon reopening in two months' time, you can expect a delectable menu featuring a wide variety of authentic dim sum delicacies, alongside a 
               selection of traditional Chinese dishes.<br /><br />
+              </p>
+              <p>
               In addition to the tantalizing flavors, we are committed to ensuring a healthier dining experience for our valued patrons. Our new menu will feature a range of 
               nutritious options crafted with the freshest ingredients, allowing you to indulge in delicious Chinese cuisine while maintaining a balanced lifestyle.<br /><br />
+              </p>
+              <p>
               We would like to express our sincere gratitude for your continued support throughout the years. Your loyalty and patronage have been invaluable to us, and we are 
               excited to serve you in our revitalized establishment.<br /><br />
-              Please stay tuned for updates on our reopening date, which we will share through our website and social media channels. If you have any questions or need 
-              further assistance, our team is here to help. Feel free to reach out to us via phone or email.<br /><br />
+              </p>
+              <p>
+              Please stay tuned for updates on our reopening date, which we will share through our website and <a className='innerFive' href="https://www.facebook.com/MikadoJapaneseSteakhouse/">facebook page</a>. If you have any questions or need 
+              further assistance, our team is here to help. Feel free to reach out to us via <a className="innerFive" href="tel:9042608860">phone</a> or <a className="innerFive" href="mailto: mikado@mikado-jax.com">email</a>.<br /><br />
+              </p>
+              <p>
               Thank you once again for your unwavering support. We eagerly look forward to welcoming you back to Mikado soon.
+              </p>
             </p>
       {/* <div className='swiperCss'>
         <Swiper
